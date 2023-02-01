2acb61b20177d27958b412caaa50e221e135dfb6
abhargkeshari <abhargkeshari@deqode.com>
2023-02-01 19:55:14 +0530

    to-do made 4 components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react'
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+
+
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Exercise',
+        time: '9 AM'
+    },
+    {
+        id: 2,
+        text: 'Assignment',
+        time: '12 PM'
+    },
+    {
+        id:3,
+        text: 'Meet XYZ',
+        time: '8 PM'
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
