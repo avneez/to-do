7e9edbb48793aa212150b1cd9158c83a2645da33
abhargkeshari <abhargkeshari@deqode.com>
2023-02-03 16:40:02 +0530

    added Add Task function using useState

--- src/App.js
+++ src/App.js
@@ -25,6 +25,13 @@ function App() {
     }
   ]);
 
+  //Adding task
+  const addTask=(task)=>{
+    const id = Math.floor(Math.random() * 10000) +1;
+    const newTask = { id, ...task}
+    setTasks([...tasks,newTask]);
+  }
+
 
   const deleteTask = (id)=>{
     setTasks(tasks.filter((task)=> task.id!==id))
@@ -34,7 +41,7 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <AddTask/>
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0? (<Tasks tasks={tasks} onDelete={deleteTask}/>) : ('No Any Tasks')}
     </div>
   );
